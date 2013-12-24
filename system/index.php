85a4173b90947f17a02796f02dc00edb5255b047
robert <danwguy@gmail.com>
2013-12-24 15:23:17 -0800

    Added in the check for 404 or 500 with proper notification

--- system/index.php
+++ system/index.php
@@ -1,7 +1,16 @@
 <?php
-$attempt = $_SERVER['REDIRECT_QUERY_STRING'];
-$pieces = explode("=", $attempt);
-$path = end($pieces);
+//echo "<pre>";
+//print_r($_SERVER);
+//echo "</pre>";
+$str = $_SERVER['REDIRECT_QUERY_STRING'];
+$str_parts = explode("&", $str);
+$post = array();
+foreach($str_parts as $data) {
+    $mine = explode("=", $data);
+    $post[$mine[0]] = $mine[1];
+}
+$type = $post['error'];
+$path = $post['request'];
 $path_pieces = explode("/", $path);
 $file = end($path_pieces);
 $class = substr($file, 0, -4);
@@ -18,16 +27,29 @@ $url .= "/".$parts[1];
 <body>
 <div class="wrapper">
     <div class="head-img">
-        <img src="../assets/img/bad-request-header.png" />
+        <img src="../assets/img/<?php echo ($type == 'script-access') ? 'bad-request-header' : '500-bad-request-header'; ?>.png" />
     </div>
     <div class="header box">
         <div class="header-title">Bad Request Encountered</div>
-        <p>It seems you are trying to access a file directly, sorry but this is not allowed.</p>
+        <div class="title-sub">
+        <p>Bad request, bad, bad request. You naughty little request you.</p>
+        <?php
+        echo ($type == 'script-access')
+            ? "<p>It seems you are trying to access a file directly, sorry but that is not allowed</p>"
+            : "<p>It seems like you are trying to get a file that doesn't exist</p>";
+            ?>
+        </div>
     </div>
     <div class="explanation box">
         <p class="dynamic-reason">
-            In order to access the class <?php echo $class; ?>, you will need to go through the framework and
-            make the proper header requests. We do not allow direct script access.
+            <?php
+            echo ($type == 'script-access')
+                ? "In order to access the class ".$class.", you will need to go through the framework and
+                    make the proper header requests. We do not allow direct script access."
+                : "I know you were looking for ".$file." but it isn't here. Check the url and make sure
+                    there aren't any typos or anything. You really should be letting the framework get this file for
+                    you, instead of trying to manually grab it.";
+            ?>
         </p>
         <p> If you feel like you have reached this page in error and you have made the correct requests, you should
             double check your assumption <a href="http://github.com/danwguy/OOF/wiki" target="_blank">Here</a>.<br />
